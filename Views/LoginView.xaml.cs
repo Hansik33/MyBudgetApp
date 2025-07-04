a78b51bbc813101e508256448b23ccacd713d66d
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2025-07-04 22:16:52 +0200

    feat(auth): support Enter key in auth forms

--- Views/LoginView.xaml.cs
+++ Views/LoginView.xaml.cs
@@ -1,8 +1,10 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using MyBudgetApp.Interfaces;
 using MyBudgetApp.ViewModels;
+using Windows.System;
 
 namespace MyBudgetApp.Views
 {
@@ -20,6 +22,24 @@ namespace MyBudgetApp.Views
         private void LoginView_Loaded(object sender, RoutedEventArgs e) => _dialogService.SetXamlRoot(XamlRoot);
 
         private LoginViewModel ViewModel => (LoginViewModel)DataContext;
+
+        private void Enter_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter && DataContext is LoginViewModel vm)
+            {
+                if (sender is TextBox textBox)
+                {
+                    var bindingExpr = textBox.GetBindingExpression(TextBox.TextProperty);
+                    bindingExpr?.UpdateSource();
+                }
+
+                if (sender is PasswordBox passwordBox)
+                    vm.Password = passwordBox.Password;
+
+                if (vm.LoginCommand.CanExecute(null))
+                    vm.LoginCommand.Execute(null);
+            }
+        }
 
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
