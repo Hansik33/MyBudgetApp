38f39e64b9289b0f3f47d6010d92933a441676c5
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2025-07-11 13:30:27 +0200

    feat(transaction): show category, payment, and date info

--- ViewModels/Dashboard/TransactionViewModel.cs
+++ ViewModels/Dashboard/TransactionViewModel.cs
@@ -1,28 +1,35 @@
 ﻿using MyBudgetApp.Enums;
 using MyBudgetApp.Models;
 using System;
+using System.Globalization;
 
 namespace MyBudgetApp.ViewModels.Dashboard
 {
     public class TransactionViewModel(Transaction transaction)
     {
         private readonly Transaction _transaction = transaction;
+        private static readonly CultureInfo Culture = new("pl-PL");
 
-        public TransactionType Type =>
-            Enum.TryParse<TransactionType>(_transaction.Type, out var type)
-                ? type
-                : TransactionType.Expense;
+        public TransactionType transactionType =>
+        Enum.TryParse<TransactionType>(_transaction.Type, true, out var type)
+        ? type
+        : TransactionType.Expense;
 
-        public string TypeText => Type switch
+        public string Type => transactionType switch
         {
             TransactionType.Expense => "Wydatek",
             TransactionType.Income => "Przychód",
             _ => throw new NotImplementedException()
         };
 
+        public string Category => _transaction.CategoryName;
         public decimal Amount => _transaction.Amount;
+        public string PaymentMethod => _transaction.PaymentMethod;
         public string Description => _transaction.Description;
+
         public DateTime Date => _transaction.Date;
-        public string CategoryName => _transaction.CategoryName;
+        public int Year => _transaction.Date.Year;
+        public string Month => Culture.TextInfo.ToTitleCase(
+            _transaction.Date.ToString("MMMM", Culture));
     }
 }
