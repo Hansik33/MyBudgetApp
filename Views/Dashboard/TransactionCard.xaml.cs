ea8635af654f95b69394fed8f8860fa063191d68
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2025-07-11 18:15:41 +0200

    refactor(dashboard): extract TransactionCard to UserControl

---
+++ Views/Dashboard/TransactionCard.xaml.cs
@@ -0,0 +1,9 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace MyBudgetApp.Views.Dashboard
+{
+    public sealed partial class TransactionCard : UserControl
+    {
+        public TransactionCard() => InitializeComponent();
+    }
+}
