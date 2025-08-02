32fbe4ca1bb2d203ad290458350cfd9ca7dfd278
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2025-08-02 08:30:15 +0200

    feat(dialog): focus in Transaction dialog

--- Views/Dashboard/Dialogs/AddTransactionDialog.xaml.cs
+++ Views/Dashboard/Dialogs/AddTransactionDialog.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace MyBudgetApp.Views.Dashboard.Dialogs
@@ -5,5 +6,12 @@ namespace MyBudgetApp.Views.Dashboard.Dialogs
     public sealed partial class AddTransactionDialog : ContentDialog
     {
         public AddTransactionDialog() => InitializeComponent();
+
+        private void ContentDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            AmountTextBox.Focus(FocusState.Programmatic);
+            AmountTextBox.SelectionStart = AmountTextBox.Text.Length;
+            AmountTextBox.SelectionLength = 0;
+        }
     }
 }
