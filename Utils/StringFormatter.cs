3f113cca50d15f9c67242f9819ce8c832db2350e
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2025-08-03 12:05:23 +0200

    feat(utils): add StringFormatter utility class

---
+++ Utils/StringFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyBudgetApp.Utils
+{
+    public static partial class StringFormatter
+    {
+        [GeneratedRegex(@"\s+")]
+        private static partial Regex WhiteSpaceRegex();
+
+        public static string Format(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return string.Empty;
+
+            input = input.Trim();
+            input = WhiteSpaceRegex().Replace(input, " ");
+            return char.ToUpper(input[0]) + input[1..];
+        }
+    }
+}
--- ViewModels/Dashboard/Dialogs/AddCategoryDialogViewModel.cs
+++ ViewModels/Dashboard/Dialogs/AddCategoryDialogViewModel.cs
@@ -1,22 +1,14 @@
-﻿namespace MyBudgetApp.ViewModels.Dashboard.Dialogs
+﻿using MyBudgetApp.Utils;
+
+namespace MyBudgetApp.ViewModels.Dashboard.Dialogs
 {
     public partial class AddCategoryDialogViewModel : BaseViewModel
     {
         private string _categoryName = string.Empty;
-        private static string FormatCategoryName(string input)
-        {
-            input = input.Trim();
-            input = MyRegex().Replace(input, " ");
-            return char.ToUpper(input[0]) + input[1..];
-        }
-
         public string CategoryName
         {
             get => _categoryName;
-            set => SetProperty(ref _categoryName, FormatCategoryName(value));
+            set => SetProperty(ref _categoryName, StringFormatter.Format(value));
         }
-
-        [System.Text.RegularExpressions.GeneratedRegex(@"\s+")]
-        private static partial System.Text.RegularExpressions.Regex MyRegex();
     }
 }
