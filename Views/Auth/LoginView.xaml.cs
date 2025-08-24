b9319d9f78cb61ca3ddc77d60887121d36c36e6a
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2025-08-24 12:35:21 +0200

    feat(auth): focus username on dialog load

--- Views/Auth/LoginView.xaml.cs
+++ Views/Auth/LoginView.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace MyBudgetApp.Views.Auth
@@ -5,5 +6,12 @@ namespace MyBudgetApp.Views.Auth
     public sealed partial class LoginView : UserControl
     {
         public LoginView() => InitializeComponent();
+
+        private void ContentDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            UsernameTextBox.Focus(FocusState.Programmatic);
+            UsernameTextBox.SelectionStart = UsernameTextBox.Text.Length;
+            UsernameTextBox.SelectionLength = 0;
+        }
     }
 }
--- Views/Auth/RegisterView.xaml.cs
+++ Views/Auth/RegisterView.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace MyBudgetApp.Views.Auth
@@ -5,5 +6,12 @@ namespace MyBudgetApp.Views.Auth
     public sealed partial class RegisterView : UserControl
     {
         public RegisterView() => InitializeComponent();
+
+        private void ContentDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            UsernameTextBox.Focus(FocusState.Programmatic);
+            UsernameTextBox.SelectionStart = UsernameTextBox.Text.Length;
+            UsernameTextBox.SelectionLength = 0;
+        }
     }
 }
